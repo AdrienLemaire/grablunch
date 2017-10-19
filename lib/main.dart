504d3aff8456a66bfd409c003250f6c10617f809
Adrien Lemaire <lemaire.adrien@gmail.com>
2017-10-19 14:47:33 +0900

    init repo

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Friendlychat")
+      ),
+    );
+  }
+}
