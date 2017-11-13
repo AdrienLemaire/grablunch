98abf3324fdb757811fd0772495c9ff643ff5b01
Adrien Lemaire <lemaire.adrien@gmail.com>
2017-11-13 12:09:09 +0900

    saving wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+import 'dart:math';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'dart:async';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 // Debug packages
 // import 'package:flutter/services.dart';
@@ -39,14 +45,13 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
-  final List<ChatMessage> _messages = <ChatMessage>[];
+class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
   bool _isComposing = false;
+  final reference = FirebaseDatabase.instance.reference().child('messages');
 
   @override
   Widget build(BuildContext context) {
-    //debugger();
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendlychat"),
@@ -57,12 +62,18 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
         child: new Column(
           children: <Widget>[
             new Flexible(
-              child: new ListView.builder(
+              child: new FirebaseAnimatedList(
+                query: reference,
+                sort: (a, b) => b.key.compareTo(a.key),
                 padding: new EdgeInsets.all(8.0),
                 reverse: true,
-                itemBuilder: (_, int index) => _messages[index],
-                itemCount: _messages.length,
-              )
+                itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation) {
+                  return new ChatMessage(
+                    snapshot: snapshot,
+                    animation: animation,
+                  );
+                },
+              ),
             ),
             new Divider(height: 1.0),
             new Container(
@@ -85,46 +96,52 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
       data: new IconThemeData(color: Theme.of(context).accentColor),
       child: new Container(
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
-          children: <Widget>[
-            new Flexible(
-              child: new TextField(
-                controller: _textController,
-                onChanged: (String text) {
-                  setState(() {
-                    _isComposing = text.length > 0;
-                  });
-                },
-                onSubmitted: _handleSubmitted,
-                decoration: new InputDecoration.collapsed(
-                  hintText: "Send a message"),
-              ),
+        child: new Row(children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+              icon: new Icon(Icons.photo_camera),
+              onPressed: () async {
+                await _ensureLoggedIn();
+                File imageFile = await ImagePicker.pickImage();
+                int random = new Random().nextInt(100000);
+                StorageReference ref = FirebaseStorage.instance.ref().child("image_$random.jpg");
+                StorageUploadTask uploadTask = ref.put(imageFile);
+                Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                _sendMessage(imageUrl: downloadUrl.toString());
+              }
             ),
-            new Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: Theme.of(context).platform == TargetPlatform.iOS ?
-                new CupertinoButton(
-                  child: new Text("Send"),
-                  onPressed: _isComposing
-                    ? () => _handleSubmitted(_textController.text) : null,
-                ) :
-                new IconButton(
-                  icon: new Icon(Icons.send),
-                  onPressed: _isComposing ?
-                    () => _handleSubmitted(_textController.text) : null,
-                ),
+          ),
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              onChanged: (String text) {
+                setState(() {
+                  _isComposing = text.length > 0;
+                });
+              },
+              onSubmitted: _handleSubmitted,
+              decoration: new InputDecoration.collapsed(
+                hintText: "Send a message"),
+            ),
+          ),
+          new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: Theme.of(context).platform == TargetPlatform.iOS ?
+              new CupertinoButton(
+                child: new Text("Send"),
+                onPressed: _isComposing
+                  ? () => _handleSubmitted(_textController.text) : null,
+              ) :
+              new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: _isComposing ?
+                  () => _handleSubmitted(_textController.text) : null,
               ),
-          ]
-        )
+          ),
+        ])
       )
     );
-  }
-
-  @override
-  void dispose() {
-    for (ChatMessage message in _messages)
-      message.animationController.dispose();
-    super.dispose();
   }
 
   Future<Null> _handleSubmitted(String text) async {
@@ -136,18 +153,13 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     _sendMessage(text: text);
   }
 
-  void _sendMessage({ String text }) {
-    ChatMessage message = new ChatMessage(
-      text: text,
-      animationController: new AnimationController(
-        duration: new Duration(milliseconds: 700),
-        vsync: this,
-      ),
-    );
-    setState(() {
-      _messages.insert(0, message);
+  void _sendMessage({ String text, String imageUrl }) {
+    reference.push().set({
+      'text': text,
+      'imageUrl': imageUrl,
+      'senderName': googleSignIn.currentUser.displayName,
+      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
     });
-    message.animationController.forward();
     analytics.logEvent(name: 'send_message');
   }
 
@@ -174,15 +186,15 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
 
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text, this.animationController});
-  final String text;
-  final AnimationController animationController;
+  ChatMessage({this.snapshot, this.animation});
+  final DataSnapshot snapshot;
+  final Animation animation;
 
   //@override
   Widget build(BuildContext context) {
     return new SizeTransition(
       sizeFactor: new CurvedAnimation(
-        parent: animationController,
+        parent: animation,
         curve: Curves.easeOut
       ),
       axisAlignment: 0.0,
@@ -194,21 +206,24 @@ class ChatMessage extends StatelessWidget {
             new Container(
               margin: const EdgeInsets.only(right: 16.0),
               child: new CircleAvatar(
-                child: new CircleAvatar(
-                  backgroundImage: new NetworkImage(
-                    googleSignIn.currentUser.photoUrl
-                  )
-                )
-              ),
+                backgroundImage: new NetworkImage(
+                  snapshot.value['senderPhotoUrl']
+                ),
+              )
             ),
             new Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                new Text(googleSignIn.currentUser.displayName,
+                new Text(snapshot.value['senderName'],
                          style: Theme.of(context).textTheme.subhead),
                 new Container(
                   margin: const EdgeInsets.only(top: 5.0),
-                  child: new Text(text),
+                  child: snapshot.value['imageUrl'] != null ?
+                    new Image.network(
+                      snapshot.value['imageUrl'],
+                      width: 250.0,
+                    ) :
+                    new Text(snapshot.value['text']),
                 ),
               ],
             ),
@@ -218,7 +233,6 @@ class ChatMessage extends StatelessWidget {
     );
   }
 }
-
 
 final ThemeData kIOSTheme = new ThemeData(
   primarySwatch: Colors.orange,
