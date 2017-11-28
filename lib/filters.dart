97a1704aac6c35d2632cd2a601d36efa25fe067b
Adrien Lemaire <lemaire.adrien@gmail.com>
2017-11-28 14:07:42 +0900

    set up mockito, start filterToday test

--- lib/filters.dart
+++ lib/filters.dart
@@ -1,9 +1,11 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/firebase_database.dart'
+    show DatabaseReference, Query;
 
 Query filterToday(DatabaseReference reference) {
   DateTime now = new DateTime.now();
   DateTime today = new DateTime(now.year, now.month, now.day);
   DateTime tomorrow = new DateTime(now.year, now.month, now.day + 1);
+  debugPrint(reference);
   return reference
       .orderByChild('date')
       .startAt(today.millisecondsSinceEpoch)
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,8 +1,23 @@
 import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:firebase_database/firebase_database.dart'
+    show DatabaseReference;
+
+import 'package:grablunch/filters.dart' show filterToday;
+
+class MockDatabaseReference extends Mock implements DatabaseReference {}
 
 void main() {
-  test('my first unit test', () {
-    var answer = 42;
-    expect(answer, 42);
+  test('filterToday', () {
+    MockDatabaseReference reference = new MockDatabaseReference();
+    DateTime now = new DateTime.now();
+    DateTime today = new DateTime(now.year, now.month, now.day);
+    DateTime tomorrow = new DateTime(now.year, now.month, now.day + 1);
+    filterToday(reference);
+    verifyInOrder([
+      reference.orderByChild('date'),
+      reference.startAt(today.millisecondsSinceEpoch),
+      reference.endAt(tomorrow.millisecondsSinceEpoch),
+    ]);
   });
 }
